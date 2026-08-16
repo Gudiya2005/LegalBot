f39957106a891f8a72709bf0251e75fdc06710a6
Gudiya Kumari Sharma <149879255+Gudiya2005@users.noreply.github.com>
2026-08-16 21:18:42 +0530

    Delete backend/test_retriever.py

--- backend/test_retriever.py
+++
@@ -1,8 +0,0 @@
-from app.rag.retriever import retrieve_knowledge
-
-question = "Someone pretending to be a police officer is asking me to transfer money."
-
-knowledge = retrieve_knowledge(question)
-
-print("=" * 50)
-print(knowledge)
